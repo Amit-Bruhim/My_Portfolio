6086a39b5fb54695d5ced260300430f41a3f1d9f
amitbbbb <126574686+amitbbbb@users.noreply.github.com>
2025-10-01 21:50:00 +0300

    hero section is ready

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,3 +1,4 @@
+import { ArrowDown } from "lucide-react"
 
 
 export const HeroSection = () => {
@@ -13,12 +14,22 @@ export const HeroSection = () => {
                 </h1>
 
             <p className="text-lg md:text-xl text-muted-foreground max-w-2xl mx-auto opacity-0 animate-fade-in-delay-4">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore itaque sed minima iure,
-                vero fugit neque veniam repellendus ipsum in dolorem tempora veritatis asperiores et natus optio vitae nobis possimus.
+                I am an ambitious and driven third-year Computer Science student at Bar-Ilan University, with academic excellence, eager to gain hands-on experience
+                in software development and seeking part-time opportunities.
             </p>
+        <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+            <a href="#projects" className="cosmic-button">
+                View My Work
+            </a>
+        </div>
 
             </div>
         </div>
+
+    <div className="absolute bottom-8 left-1/2 transform-translate-x-1/2 flex flex-col items-center animate-bounce">
+        <span className="text-sm text-muted-foreground mb-2"> Scroll</span>
+        <ArrowDown className="h-5 w-5 text-primary"/>
+    </div>
 
     </section>
 }
