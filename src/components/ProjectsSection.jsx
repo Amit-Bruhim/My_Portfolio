6123af30a967e240c9adb64ccbc0bce66abd8604
amitbbbb <126574686+amitbbbb@users.noreply.github.com>
2025-10-02 21:20:46 +0300

    projects section is ready

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,4 +1,4 @@
-import { ExternalLink, Github } from "lucide-react"
+import { ArrowRight, ExternalLink, Github } from "lucide-react"
 
 const projects = [
     {
@@ -21,7 +21,7 @@ const projects = [
         githuburl: "#",
     },
 
-     {
+    {
         id: 3,
         title: "Netflix clone",
         description: "Coming soon...",
@@ -39,50 +39,63 @@ export const ProjectsSection = () => {
                 Featured <span className="text-primary"> Projects </span>
             </h2>
 
-            <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+            <p className="text-center text-muted-foreground mb-4 max-w-2xl mx-auto">
                 Here are some of my recent projects.<br />
                 Each project was carefully crafted with attention to detail, performance, and user experience.
             </p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">  
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {projects.map((project, key) => (
                     <div
                         key={key}
                         className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover flex flex-col h-full"
-                    > 
-                        <div className="h-48 overflow-hidden">
+                    >
+                        <div className="h-38 overflow-hidden">
                             <img
                                 src={project.image}
                                 alt={project.title}
                                 className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
                             />
                         </div>
-
-                        <div className="pt-6 pl-6">
-                            <div className="flex flex-wrap gap-2 mb-4">
-                                {project.tags.map((tag) => (
-                                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-primary/20 text-secondary-foreground">
-                                        {tag}
-                                    </span>
-                                ))}
+                        <div className="p-4">
+                            <div className="pl-6">
+                                <div className="flex flex-wrap gap-2 mb-2">
+                                    {project.tags.map((tag) => (
+                                        <span className="px-2 py-1 text-xs font-medium border rounded-full bg-primary/20 text-secondary-foreground">
+                                            {tag}
+                                        </span>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
 
-                        <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
-                        <p className="text-muted-foreground mb-4 text-sm">{project.description}</p>
-                        <div className="flex items-center justify-between ml-2">
-                            <div className="flex space-x-3">
-                               <a>
-                                  <ExternalLink/>
-                               </a>
-                               <a>
-                                  <Github/>
-                               </a>
-
+                            <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
+                            <p className="text-muted-foreground mb-4 text-sm">{project.description}</p>
+                            <div className="flex items-center justify-between ml-2">
+                                <div className="flex space-x-3">
+                                    <a href={project.demourl}
+                                        target="_blank"
+                                        className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                                        <ExternalLink />
+                                    </a>
+                                    <a href={project.githuburl}
+                                        target="_blank"
+                                        className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                                        <Github />
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 ))}
+            </div>
+            <div className="text-center mt-4">
+                <a
+                    className="cosmic-button w-fit flex items-center mx-auto gap-2"
+                    target="_blank"
+                    href="#"
+                >
+                    Check My Github <ArrowRight size={16} />
+                </a>
             </div>
         </div>
 
