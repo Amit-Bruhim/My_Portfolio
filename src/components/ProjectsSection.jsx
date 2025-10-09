f198024ef7a786f7fda7573b9b819e1216e4e32c
amitbbbb <126574686+amitbbbb@users.noreply.github.com>
2025-10-09 23:08:51 +0300

    added swiper

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,4 +1,8 @@
 import { ArrowRight, Github } from "lucide-react"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const projects = [
     {
@@ -39,7 +43,7 @@ const projects = [
 ]
 
 export const ProjectsSection = () => {
-    return <section id="projects" className="py-24 px-4 relative">
+    return <section id="projects" className="py-20 px-4 relative">
         <div className="container mx-auto max-w-5xl">
             <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
                 Featured <span className="text-primary"> Projects </span>
@@ -50,55 +54,67 @@ export const ProjectsSection = () => {
                 Each project was carefully crafted with attention to detail, performance, and user experience.
             </p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                {projects.map((project, key) => (
-                    <div
-                        key={key}
-                        className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover flex flex-col h-full"
-                    >
-                        <div className="h-38 overflow-hidden">
-                            <img
-                                src={project.image}
-                                alt={project.title}
-                                className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
-                            />
-                        </div>
-                        <div className="p-4">
-                            <div className="pl-6">
-                                <div className="flex flex-wrap gap-2 mb-2">
-                                    {project.tags.map((tag) => (
-                                        <span className="px-2 py-1 text-xs font-medium border rounded-full bg-primary/20 text-secondary-foreground">
-                                            {tag}
-                                        </span>
-                                    ))}
-                                </div>
+            <Swiper modules={[Navigation]}
+                navigation
+                spaceBetween={30}
+                slidesPerView={1}
+                breakpoints={{
+                    768: { slidesPerView: 2 },
+                    1024: { slidesPerView: 3 },
+                }}
+                className="my-1 relative"
+            >
+            {projects.map((project, key) => (
+                <SwiperSlide key={project.id}>
+                <div
+                    key={key}
+                    className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover flex flex-col h-full min-h-[350px]"
+                >
+                    <div className="h-38 overflow-hidden">
+                        <img
+                            src={project.image}
+                            alt={project.title}
+                            className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                        />
+                    </div>
+                    <div className="p-4">
+                        <div className="pl-6">
+                            <div className="flex flex-wrap gap-2 mb-2">
+                                {project.tags.map((tag) => (
+                                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-primary/20 text-secondary-foreground">
+                                        {tag}
+                                    </span>
+                                ))}
                             </div>
+                        </div>
 
-                            <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
-                            <p className="text-muted-foreground mb-4 text-sm">{project.description}</p>
-                            <div className="flex items-center justify-between ml-2">
-                                <div className="flex space-x-3">
-                                    <a href={project.githuburl}
-                                        target="_blank"
-                                        className="text-foreground/80 hover:text-primary transition-colors duration-300">
-                                        <Github />
-                                    </a>
-                                </div>
+                        <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
+                        <p className="text-muted-foreground mb-4 text-sm">{project.description}</p>
+                        <div className="flex items-center justify-between ml-2">
+                            <div className="flex space-x-3">
+                                <a href={project.githuburl}
+                                    target="_blank"
+                                    className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                                    <Github />
+                                </a>
                             </div>
                         </div>
                     </div>
-                ))}
-            </div>
-            <div className="text-center mt-4">
-                <a
-                    className="cosmic-button w-fit flex items-center mx-auto gap-2"
-                    target="_blank"
-                    href="https://github.com/Amit-Bruhim"
-                >
-                    Check My Github <ArrowRight size={16} />
-                </a>
-            </div>
+                </div>
+                </SwiperSlide>
+            ))}
+            </Swiper>
         </div>
+        <div className="text-center mt-4">
+            <a
+                className="cosmic-button w-fit flex items-center mx-auto gap-2"
+                target="_blank"
+                href="https://github.com/Amit-Bruhim"
+            >
+                Check My Github <ArrowRight size={16} />
+            </a>
+        </div>
+    
 
-    </section>
+    </section >
 }
